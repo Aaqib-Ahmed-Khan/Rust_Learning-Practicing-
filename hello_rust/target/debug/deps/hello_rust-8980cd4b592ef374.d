4f578408c067670209106ad364fdba1aedefb1df
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-09-19 23:54:47 +0500

    Add initial Rust project

---
+++ hello_rust/target/debug/deps/hello_rust-8980cd4b592ef374.d
@@ -0,0 +1,5 @@
+/home/aaqib/Rust_practice/hello_rust/target/debug/deps/hello_rust-8980cd4b592ef374.d: src/main.rs
+
+/home/aaqib/Rust_practice/hello_rust/target/debug/deps/hello_rust-8980cd4b592ef374: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/home/aaqib/Rust_practice/hello_rust/target/debug/hello_rust: /home/aaqib/Rust_practice/hello_rust/src/main.rs
